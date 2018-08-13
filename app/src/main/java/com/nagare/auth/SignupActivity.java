426c9cdec5300e42075523af971130dcb9bcaa47
dionsaputra <dionsaputra666@gmail.com>
2018-08-13 21:22:17 +0700

    Add sign up view

---
+++ app/src/main/java/com/nagare/auth/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.nagare.auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nagare.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auth_signup);
+    }
+}
