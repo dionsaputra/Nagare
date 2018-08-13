b5798f584deafc7f2e181633754d34ebd7419591
dionsaputra <dionsaputra666@gmail.com>
2018-08-13 21:10:21 +0700

    Add shared element transition

--- app/src/main/java/com/nagare/auth/LoginActivity.java
+++ app/src/main/java/com/nagare/auth/LoginActivity.java
@@ -19,15 +19,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,7 +62,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.auth_login);
 
 //        // Set up the login form.
 //        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
--- app/src/main/java/com/nagare/auth/OpeningActivity.java
+++ app/src/main/java/com/nagare/auth/OpeningActivity.java
@@ -1,6 +1,10 @@
 package com.nagare.auth;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +15,12 @@ import com.nagare.MainActivity;
 import com.nagare.R;
 
 public class OpeningActivity extends AppCompatActivity {
-    private final int OPENING_TIME = 2000;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opening);
+        setContentView(R.layout.auth_opening);
 
         loadOpeningBackground();
         setNagareLogoAction();
@@ -34,19 +38,30 @@ public class OpeningActivity extends AppCompatActivity {
      * Define action to do when nagare logo clicked.
      */
     private void setNagareLogoAction() {
-        ImageView nagareLogo = (ImageView) findViewById(R.id.nagare_logo);
+        final ImageView nagareLogo = (ImageView) findViewById(R.id.nagare_logo);
         nagareLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (hasLogin()) {
-                    Intent intent = new Intent(OpeningActivity.this, MainActivity.class);
-                    startActivity(intent);
+                    startNewActivity(MainActivity.class, nagareLogo, R.string.nagare_logo);
                 } else {
-                    Intent intent = new Intent(OpeningActivity.this, LoginActivity.class);
-                    startActivity(intent);
+                    startNewActivity(LoginActivity.class, nagareLogo, R.string.nagare_logo);
                 }
             }
         });
+    }
+
+    /**
+     * Start new activity by sharedElement transition.
+     * @param targetActivity        next activity to start
+     * @param sharedElement         shared element between two activity
+     * @param sharedElementResId    string resource of sharedElement
+     */
+    private void startNewActivity(Class targetActivity, View sharedElement, int sharedElementResId) {
+        Intent intent = new Intent(context, targetActivity);
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                (Activity) context, sharedElement, getString(sharedElementResId));
+        startActivity(intent, options.toBundle());
     }
 
     /**
