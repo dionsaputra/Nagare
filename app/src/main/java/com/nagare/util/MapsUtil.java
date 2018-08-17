69c4ed4bff0e5fdf8fa929d09ecfb9342d3f696f
dionsaputra <dionsaputra666@gmail.com>
2018-08-17 23:14:25 +0700

    Add Maps Util

---
+++ app/src/main/java/com/nagare/util/MapsUtil.java
@@ -0,0 +1,22 @@
+package com.nagare.util;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.nagare.R;
+
+public class MapsUtil{
+
+    public static void showMaps(Context context, SupportMapFragment mapFragment) {
+        mapFragment.getMapAsync((OnMapReadyCallback) context);
+    }
+
+}
