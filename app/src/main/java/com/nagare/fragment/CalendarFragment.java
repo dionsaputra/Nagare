957a0e51ade6b4a5e8f1232ef73aea1dec915f8c
dionsaputra <dionsaputra666@gmail.com>
2018-08-25 18:22:14 +0700

    add calendar view

--- app/src/main/java/com/nagare/fragment/CalendarFragment.java
+++ app/src/main/java/com/nagare/fragment/CalendarFragment.java
@@ -13,54 +13,60 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.nagare.R;
 
 public class CalendarFragment extends Fragment {
 
-    ActionBar calendarActionBar;
+    View rootView;
+    CalendarView mainCalendar;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    public void setupActionBar() {
-        android.support.v7.app.ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
-        if (actionBar == null) return;
-
-        actionBar.setCustomView(R.layout.custom_action_bar);
-        Spinner actionBarSpinner = actionBar.getCustomView().findViewById(R.id.custom_action_bar_spinner);
-        ArrayAdapter mapsSpinnerAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.calendar_options, R.layout.title_spinner);
-        mapsSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        actionBarSpinner.setAdapter(mapsSpinnerAdapter);
-        actionBar.setDisplayOptions(android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM | android.support.v7.app.ActionBar.DISPLAY_SHOW_HOME);
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_calendar, container, false);
+        rootView = inflater.inflate(R.layout.fragment_calendar, container, false);
+        initComponent();
         return rootView;
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.clear();
-        inflater.inflate(R.menu.calendar_menu, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
+    private void initComponent() {
+        mainCalendar = rootView.findViewById(R.id.cv_main_calendar);
+        mainCalendar.setDate(System.currentTimeMillis(),false,true);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.acara_action) {
-            showAcara();
-        } else {
-            showTemuLurah();
-        }
-        return true;
+        mainCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + " " + month + " " + year;
+                Toast.makeText(getContext(), date, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+//
+//    @Override
+//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        menu.clear();
+//        inflater.inflate(R.menu.calendar_menu, menu);
+//        super.onCreateOptionsMenu(menu, inflater);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.acara_action) {
+//            showAcara();
+//        } else {
+//            showTemuLurah();
+//        }
+//        return true;
+//    }
 
     public void showTemuLurah() {
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.temu_lurah));
