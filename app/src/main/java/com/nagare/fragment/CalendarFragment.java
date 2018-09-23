4aa0c1eb715b5fe45b4f2c66bdfb0987549ff152
dionsaputra <dionsaputra666@gmail.com>
2018-09-24 05:35:14 +0700

    Add navigation

--- app/src/main/java/com/nagare/fragment/CalendarFragment.java
+++ app/src/main/java/com/nagare/fragment/CalendarFragment.java
@@ -7,9 +7,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +24,7 @@ import com.nagare.base.BaseMainFragment;
 import com.nagare.model.Kalender;
 import com.nagare.util.DataUtil;
 
+import com.nagare.util.ViewUtil;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
@@ -29,12 +33,15 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CalendarFragment extends BaseMainFragment{
+public class CalendarFragment extends BaseMainFragment implements OnDateSelectedListener{
 
     private MaterialCalendarView mainCalendar;
     private FloatingActionButton fab;
     private boolean isAcara = true;
+    private Map<Long, ArrayList<Kalender>> acaraMap = new HashMap<>();
 
     public CalendarFragment() {
         super();
@@ -44,6 +51,7 @@ public class CalendarFragment extends BaseMainFragment{
     @Override
     protected void initComponent() {
         mainCalendar = rootView.findViewById(R.id.cv_main_calendar);
+        mainCalendar.setOnDateChangedListener(this);
         fab = rootView.findViewById(R.id.fab);
     }
 
@@ -55,41 +63,6 @@ public class CalendarFragment extends BaseMainFragment{
     }
 
     private void setupMainCalendar() {
-        mainCalendar.setOnDateChangedListener(new OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull final CalendarDay date, boolean selected) {
-                ValueEventListener calendarListener = new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        boolean eventExist = false;
-                        for (DataSnapshot item : dataSnapshot.getChildren()) {
-                            Kalender kalender = item.getValue(Kalender.class);
-                            long dbDate = kalender.date;
-                            long eventDate = date.getDate().getTime();
-                            if(dbDate == eventDate) {
-                                eventExist = true;
-                                break;
-                            }
-                        }
-                        if(!eventExist) {
-                            Toast.makeText(getContext(), "doesn't event", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getContext(), "event", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                };
-                if(isAcara){
-                    DataUtil.dbAcara.addListenerForSingleValueEvent(calendarListener);
-                } else {
-                    DataUtil.dbTemuLurah.addListenerForSingleValueEvent(calendarListener);
-                }
-            }
-        });
     }
 
     private void setupFloatingButton() {
@@ -167,7 +140,15 @@ public class CalendarFragment extends BaseMainFragment{
                 ArrayList<CalendarDay> events = new ArrayList<>();
                 for (DataSnapshot item : dataSnapshot.getChildren()) {
                     Kalender kalender = item.getValue(Kalender.class);
-                    events.add(new CalendarDay(new Date(kalender.date)));
+                    events.add(new CalendarDay(new Date(kalender.getDate())));
+
+                    if (acaraMap.containsKey(kalender.getDate())) {
+                        acaraMap.get(kalender.getDate()).add(kalender);
+                    } else {
+                        ArrayList<Kalender> acaras = new ArrayList<>();
+                        acaras.add(kalender);
+                        acaraMap.put(kalender.getDate(), acaras);
+                    }
                 }
                 mainCalendar.addDecorator(new EventDecorator(Color.GREEN, events));
             }
@@ -206,4 +187,61 @@ public class CalendarFragment extends BaseMainFragment{
         isAcara = acara;
     }
 
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        final ArrayList<Kalender> acaras = acaraMap.get(date.getDate().getTime());
+        if (acaras == null) return;
+
+        LayoutInflater inflater = ((AppCompatActivity)getContext()).getLayoutInflater();
+        final View view = inflater.inflate(R.layout.detail_dialog,null);
+
+        final TextView title = view.findViewById(R.id.tv_acara_name);
+        final TextView counter = view.findViewById(R.id.tv_counter);
+        title.setText(acaras.get(0).getTitle());
+        counter.setText("1/" + String.valueOf(acaras.size()));
+
+        final ImageView leftArrow = view.findViewById(R.id.arrow_left);
+        final ImageView rightArrow = view.findViewById(R.id.arrow_right);
+
+        if (acaras.size() < 2) {
+            leftArrow.setVisibility(View.GONE);
+            rightArrow.setVisibility(View.GONE);
+        } else {
+            final int[] position = {0};
+            leftArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    position[0]--;
+                    if (0 <= position[0] && position[0] < acaras.size()) {
+                        title.setText(acaras.get(position[0]).getTitle());
+                        counter.setText(String.valueOf(position[0]+1) + "/" + String.valueOf(acaras.size()));
+                    }
+                }
+            });
+
+            rightArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    position[0]++;
+                    if (0 <= position[0] && position[0] < acaras.size()) {
+                        title.setText(acaras.get(position[0]).getTitle());
+                        counter.setText(String.valueOf(position[0]+1) + "/" + String.valueOf(acaras.size()));
+                    }
+                }
+            });
+        }
+
+        ImageView imageView = view.findViewById(R.id.iv_selected_lokasi);
+        ViewUtil.loadImage(getContext(),imageView,R.drawable.itb);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),R.style.MyAlertDialogTheme);
+        builder.setView(view)
+                .setPositiveButton("NEXT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        builder.create().show();
+    }
 }
