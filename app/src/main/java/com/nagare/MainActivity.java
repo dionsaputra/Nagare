1982146579e451659ead19d51ec0cfcde385fe99
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-09-24 22:43:34 +0700

    add name and email to header

--- app/src/main/java/com/nagare/MainActivity.java
+++ app/src/main/java/com/nagare/MainActivity.java
@@ -22,11 +22,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.nagare.adapter.SimpleFragmentPagerAdapter;
 import com.nagare.auth.LoginActivity;
 import com.nagare.fragment.CalendarFragment;
 import com.nagare.fragment.GalangDanaFragment;
 import com.nagare.fragment.MapsFragment;
+import com.nagare.model.User;
 import com.nagare.util.DataUtil;
 
 public class MainActivity extends AppCompatActivity{
@@ -93,6 +98,23 @@ public class MainActivity extends AppCompatActivity{
 
     private void setupSideNavbar(){
         mDrawerLayout = findViewById(R.id.drawer_layout);
+        final TextView header_name = findViewById(R.id.nav_header_name);
+        final TextView header_email = findViewById(R.id.nav_header_email);
+
+        Query query = DataUtil.dbUser.child(DataUtil.USER_KEY);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                header_name.setText(dataSnapshot.getValue(User.class).getName());
+                header_email.setText(dataSnapshot.getValue(User.class).getEmail());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
